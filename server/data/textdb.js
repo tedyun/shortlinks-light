921a5e2d39f75bb273ecb475c1a73ff49864afe4
Ted Yun <tedyun@users.noreply.github.com>
2019-09-23 23:04:50 -0400

    Update textdb.js

--- server/data/textdb.js
+++ server/data/textdb.js
@@ -1,5 +1,6 @@
 module.exports = {
-  hmx: "https://onlinelearning.hms.harvard.edu/dashboard",
   gcp: "https://console.cloud.google.com/compute/instances",
+  gcs: "https://console.cloud.google.com/storage/browser",
+  hmx: "https://onlinelearning.hms.harvard.edu/dashboard",
   qubit: "https://courses.edx.org/courses/BerkeleyX/CS-191x/2013_August/course/",
 };
