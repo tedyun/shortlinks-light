10bd91cc0c7771d5eef267e290ba3ec9e434ca57
Ted Yun <tedyun@gmail.com>
2019-09-24 00:53:57 +0000

    modified data

--- server/data/textdb.js
+++ server/data/textdb.js
@@ -1,3 +1,5 @@
 module.exports = {
-  hmx: "https://onlinelearning.hms.harvard.edu/dashboard",
+    hmx: "https://onlinelearning.hms.harvard.edu/dashboard",
+    gcp: "https://console.cloud.google.com/compute/instances",
+    quantum: "https://courses.edx.org/courses/BerkeleyX/CS-191x/2013_August/course/",
 };
