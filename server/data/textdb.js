4094e6eed058bc676cd70c8cb6ad3777075ad549
Ted Yun <tedyun@gmail.com>
2019-10-17 01:48:56 +0000

    adding more site data

--- server/data/textdb.js
+++ server/data/textdb.js
@@ -1,7 +1,10 @@
 module.exports = {
-  gcp: "https://console.cloud.google.com/compute/instances",
-  gcs: "https://console.cloud.google.com/storage/browser",
-  hmx: "https://onlinelearning.hms.harvard.edu/dashboard",
-  llearn: "https://www.linkedin.com/learning/",
-  qubit: "https://courses.edx.org/courses/BerkeleyX/CS-191x/2013_August/course/",
+    gcp: "https://console.cloud.google.com/compute/instances",
+    gcs: "https://console.cloud.google.com/storage/browser",
+    gscholar: "https://scholar.google.com/citations?user=KljLQpUAAAAJ",
+    genxiv: "https://www.biorxiv.org/collection/genomics",  
+    hmx: "https://onlinelearning.hms.harvard.edu/dashboard",
+    llearn: "https://www.linkedin.com/learning/",
+    qubit: "https://courses.edx.org/courses/BerkeleyX/CS-191x/2013_August/course/",
+    td: "https://docs.google.com/document/d/1Cq-tjVhh4QIeIkrt8jB-IP8-yS7SBBnTXhTKZJkHAxU/edit?usp=sharing",
 };
